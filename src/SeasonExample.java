5a55254fbd50108196a0e56cb0b8e86c3521a4da
VikDienh <viktoriia.dengina@gmail.com>
2024-10-03 22:01:37 +0300

    commit

--- src/SeasonExample.java
+++ src/SeasonExample.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SeasonExample {
+
+    public enum Season {
+        WINTER, SPRING, SUMMER, AUTUMN;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type the month");
+        String month = scanner.nextLine();
+
+        Season season;
+
+        switch (month.toLowerCase()) {
+            case "december":
+            case "january":
+            case "february":
+                season = Season.WINTER;
+                System.out.println(season);
+                break;
+            case "march":
+            case "april":
+            case "may":
+                season = Season.SPRING;
+                System.out.println(season);
+                break;
+            case "june":
+            case "july":
+            case "august":
+                season = Season.SUMMER;
+                System.out.println(season);
+                break;
+            case "september":
+            case "october":
+            case "november":
+                season = Season.AUTUMN;
+                System.out.println(season);
+                break;
+            default:
+                System.out.println("Try again please!");
+
+        }
+        scanner.close();
+    }
 }
